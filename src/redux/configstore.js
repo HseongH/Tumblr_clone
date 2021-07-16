fe8fb209151beb979848d0b209e6c14e54d98b2a
unknown <hh4518@gmail.com>
2021-07-16 20:09:41 +0900

    기본 파일 생성 및 사용 패키지 설치

---
+++ src/redux/configstore.js
@@ -0,0 +1,21 @@
+// LIBRARY
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { createBrowserHistory } from 'history';
+import thunk from 'redux-thunk';
+
+// HISTORY
+export const history = createBrowserHistory();
+
+const rootReducer = combineReducers({});
+
+const middlewares = [thunk.withExtraArgument({ history: history })];
+
+if (process.env.NODE_ENV === 'development') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
+const enhancer = applyMiddleware(...middlewares);
+let store = compose(createStore(rootReducer, enhancer));
+
+export default store;

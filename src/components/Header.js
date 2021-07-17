16fa495aeaaa9a1907ab4747ceb717e5ce25b6e2
sunm-in <sunm309@naver.com>
2021-07-17 15:43:54 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,68 +1,71 @@
 // LIBRARY
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 // STYLE
-import { flexBox, flexVer } from '../common/style';
+import { flexBox, flexVer } from "../common/style";
 
 // COMPONENTS
-import Logo from './Logo';
+import Logo from "./Logo";
 
 // ELEMENTS
-import { Input, Button, Grid, Image } from '../elements/index';
+import { Input, Button, Grid, Image } from "../elements/index";
 
 // ICON
-import HomeIcon from '@material-ui/icons/Home';
-import { FaBolt } from 'react-icons/fa';
-import PersonIcon from '@material-ui/icons/Person';
-import CreateIcon from '@material-ui/icons/Create';
+import HomeIcon from "@material-ui/icons/Home";
+import { FaBolt } from "react-icons/fa";
+import PersonIcon from "@material-ui/icons/Person";
+import CreateIcon from "@material-ui/icons/Create";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Header = (props) => {
   return (
     <HeaderStyle>
       <HeaderWrap>
-        {' '}
-        {/* flexBox함수 */}
         <Logo>
-          {' '}
-          {/* 로고에 링크 태그 들어가야됨 */}
           <Image
             src="https://seeklogo.com/images/T/tumblr-icon-logo-A42B4BE5C1-seeklogo.com.png"
             alt="tumblr logo"
           />
         </Logo>
-        <div>
-          {' '}
-          {/* div 크기 설정 */}
-          {/* span 태그로 검색 아이콘 넣기 */}
-          <Input placeholder="Tumblr 검색" />
+        <div style={{ backgroundColor: "rgba(255, 255, 255, 0.3)", display: "flex" }}>
+          <span style={{ color: "rgba(255, 255, 255, 0.3)" }}>
+            <SearchIcon fontSize="large" />
+          </span>
+
+          <Input placeholder="Tumblr 검색" width="500px" />
         </div>
       </HeaderWrap>
 
-      <HomeIcon fontSize="large" />
-      <FaBolt fontSize="26px" />
-      <PersonIcon fontSize="large" />
-      <CreateIcon fontSize="large" />
-
-      <Grid appendStyle={flexVer}>
+      <Grid appendStyle={flexVer} margin="0 0 0 35%">
         {/* 버튼 안에 아이콘 넣고 마진값 설정, 작성 버튼 크기 지정해주고 background-color 지정, 링크 태그 들어가야됨 */}
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
+        <ButtonBoxStyle>
+          <Button>
+            <HomeIcon fontSize="large" />
+          </Button>
+          <Button>
+            <FaBolt fontSize="28px" />
+          </Button>
+          <Button>
+            <PersonIcon fontSize="large" />
+          </Button>
+          <Button>
+            <CreateIcon fontSize="large" />
+          </Button>
+        </ButtonBoxStyle>
       </Grid>
     </HeaderStyle>
   );
 };
 
 const HeaderWrap = styled.div`
-  ${flexBox('left')};
+  ${flexBox("left")};
   width: 100%;
   height: 54px;
 `;
 
 const HeaderStyle = styled.header`
-  ${flexBox('space-between')};
+  ${flexBox("space-between")};
   width: 100%;
   max-width: 1716px;
   padding: 0 20px 0 22px;
@@ -73,7 +76,15 @@ const HeaderStyle = styled.header`
   z-index: 9;
 `;
 
-const IconStyle = styled.span``;
+const ButtonBoxStyle = styled.div`
+  display: flex;
+  & button {
+    margin: 0 8px;
+    color: rgba(255, 255, 255, 0.5);
+  }
+`;
+
+const SearchIconStyle = styled.div``;
 
 Header.defaultProps = {};
 

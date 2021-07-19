e8124b094f264c6876a1962cc7a271b53a1ce68b
sunm-in <sunm309@naver.com>
2021-07-19 19:18:03 +0900

    로그인, 이메일 체크, 인증 뼈대

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -8,20 +8,21 @@ import { produce } from "immer";
 // function
 
 //actions
-// const LOG_IN = "LOG_IN";
+const LOG_IN = "LOG_IN";
 const LOG_OUT = "LOG_OUT";
 const AUTH_USER = "AUTH_USER";
 const CHECK_EMAIL = "CHECK_EMAIL";
 
 // userId, nickname, profileImg
 // action creators
-// const logIn = createAction(LOG_IN, (user) => (user));
+const logIn = createAction(LOG_IN, (user) => user);
 const logOut = createAction(LOG_OUT, (user) => ({ user }));
 const authUser = createAction(AUTH_USER, (userInfo) => ({ userInfo }));
 const checkEmail = createAction(CHECK_EMAIL, (email) => ({ email }));
 
 // initialState
 const initialState = {
+  cookie: null,
   user: null,
   is_login: false,
   is_check: false,
@@ -42,6 +43,53 @@ const authUserDB = () => {
       })
       .catch((error) => {
         console.log(error);
+      });
+  };
+};
+
+const loginAction = (user) => {
+  return function (dispatch, getState, { history }) {
+    instance
+      .post("/api/login", user)
+      .then((res) => {
+        const userInfo = {
+          email: res.data.userId,
+          nickname: user.nickname,
+        };
+
+        dispatch(checkEmail(userInfo));
+        dispatch(logIn(res.data.cookie));
+
+        // setCookie(res.data.cookie);
+
+        history.push("/");
+      })
+      .catch((error) => {
+        console.log(error);
+        window.alert("아이디 또는 패스워드가 올바르지 않습니다.");
+      });
+  };
+};
+
+const loginCheck = (cookie) => {
+  return function (dispatch) {
+    if (cookie) {
+      dispatch(logIn(cookie));
+    }
+  };
+};
+
+const emailCheck = (email) => {
+  return function (dispatch) {
+    instance
+      .post("/api/login/email", { email: email })
+      .then((res) => {
+        dispatch(checkEmail(true));
+        window.alert("사용 가능한 이메일입니다.");
+      })
+      .catch((error) => {
+        dispatch(checkEmail(false));
+        window.alert("이미 사용중인 이메일입니다.");
       });
   };
 };
@@ -73,9 +121,26 @@ export default handleActions(
         draft.profileImg = action.patload.userInfo.profileImg;
       }),
 
-    [LOG_OUT]: (state, action) => produce(state, (draft) => {}),
+    [LOG_IN]: (state, action) =>
+      produce(state, (draft) => {
+        draft.cookie = action.payload.cookie;
+        draft.is_login = true;
+      }),
 
-    [CHECK_EMAIL]: (state, action) => produce(state, (draft) => {}),
+    [LOG_OUT]: (state, action) =>
+      produce(state, (draft) => {
+        // removeAuthorization();
+        draft.userId = null;
+        draft.email = null;
+        draft.nickname = null;
+        draft.cookie = null;
+        draft.is_login = false;
+      }),
+
+    [CHECK_EMAIL]: (state, action) =>
+      produce(state, (draft) => {
+        draft.is_check = action.payload.email;
+      }),
   },
   initialState
 );
@@ -85,6 +150,9 @@ const userActions = {
   checkEmail,
   signupDB,
   authUserDB,
+  loginAction,
+  loginCheck,
+  emailCheck,
 };
 
 export { userActions };

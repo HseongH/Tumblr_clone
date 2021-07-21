70737570c2defa4463564365cdb8e72d7eae0511
sunm-in <sunm309@naver.com>
2021-07-21 17:07:46 +0900

    팔로잉 뷰 완료

--- src/pages/Following.js
+++ src/pages/Following.js
@@ -12,6 +12,9 @@ import { Grid, Text, Input, Button, Image } from "../elements";
 import BlogUser from "../components/BlogUser";
 import Logo from "../components/Logo";
 
+// ICON
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
 const Following = (props) => {
   return (
     <React.Fragment>
@@ -45,7 +48,6 @@ const Following = (props) => {
             addstyle={() => {
               return css`
                 display: flex;
-                /* align-items: center;  */
               `;
             }}
           >
@@ -71,6 +73,8 @@ const Following = (props) => {
               팔로우
             </Button>
           </Grid>
+
+          {/* 구분선 -------------------------------- */}
 
           <Grid margin="2% 0 0 0">
             <Grid
@@ -127,11 +131,103 @@ const Following = (props) => {
                   5일 전 업데이트됨
                 </Text>
               </Grid>
-            </Grid>
 
-            <Grid></Grid>
+              <Text
+                margin="0 -30% 0 30%"
+                fontSize="14px"
+                color="blue"
+                addstyle={() => {
+                  return css`
+                    padding: 2px;
+                    width: 100%;
+                  `;
+                }}
+              >
+                언팔로우
+              </Text>
+
+              <MoreVertIcon />
+            </Grid>
+          </Grid>
+
+          {/* 구분선 -------------------------------- */}
+
+          <Grid margin="0 0 0 0">
+            <Grid
+              padding="12px"
+              bgColor="white"
+              addstyle={() => {
+                return css`
+                  display: flex;
+                  align-items: center;
+                `;
+              }}
+            >
+              <Image
+                addstyle={() => {
+                  return css`
+                    width: 36px;
+                  `;
+                }}
+                height="36px"
+                src="https://assets.tumblr.com/images/default_avatar/octahedron_open_128.png"
+              />
+
+              <Grid
+                magin="0 0 3% 0"
+                addstyle={() => {
+                  return css`
+                    display: flex;
+                    flex-direction: column;
+                    padding: 4px;
+                  `;
+                }}
+              >
+                <Text
+                  fontSize="13px"
+                  fontWeight="bold"
+                  addstyle={() => {
+                    return css`
+                      padding: 2px;
+                    `;
+                  }}
+                >
+                  hangulteam
+                </Text>
+
+                <Text
+                  fontSize="14px"
+                  fontWeight="300"
+                  addstyle={() => {
+                    return css`
+                      padding: 2px;
+                    `;
+                  }}
+                >
+                  5일 전 업데이트됨
+                </Text>
+              </Grid>
+
+              <Text
+                margin="0 -30% 0 30%"
+                fontSize="14px"
+                color="blue"
+                addstyle={() => {
+                  return css`
+                    padding: 2px;
+                    width: 100%;
+                  `;
+                }}
+              >
+                언팔로우
+              </Text>
+
+              <MoreVertIcon />
+            </Grid>
           </Grid>
         </Grid>
+
+        {/* 구분선 -------------------------------- */}
 
         <Grid
           width="100%"

6b30941f26cedfb624a1e37f7ee9fbb44ed18e2f
sunm-in <sunm309@naver.com>
2021-07-21 14:27:16 +0900

    mypage 생성

--- src/redux/modules/like.js
+++ src/redux/modules/like.js
@@ -4,18 +4,6 @@ import instance from '../../common/axios';
 // REDUX
 import { postActions } from './post';
 
-// ACTION
-const GET_LIKE_POST = 'GET_LIKE_POST';
-
-// ACTION CREATOR
-const getLikePost = (likeList, start) => ({ type: GET_LIKE_POST, likeList, start });
-
-// INITIAL STATE
-const initialState = {
-  list: [],
-  start: 0,
-};
-
 // MIDDLEWARE
 const addLikeDB = (postId, post) => {
   return function (dispatch) {
@@ -47,42 +35,9 @@ const removeLikeDB = (postId, post) => {
   };
 };
 
-const getLikeDB = (limit = 5) => {
-  return function (dispatch) {
-    instance
-      .get(`/api/post/like?start=0&limit=${limit + 1}`)
-      .then((res) => {
-        if (res.data.result.length < limit + 1) {
-          dispatch(getLikePost(res.data.result.null));
-          return;
-        }
-
-        if (res.data.result.length >= limit + 1) res.data.result.pop();
-
-        dispatch(getLikePost(res.data.result, limit));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-};
-
-// REDUCER
-function like(state = initialState, action) {
-  switch (action.type) {
-    case GET_LIKE_POST:
-      return { list: action.likeList, start: action.start };
-
-    default:
-      return state;
-  }
-}
-
 export const likeActions = {
   addLikeDB,
   removeLikeDB,
-  getLikeDB,
-  getLikePost,
 };
 
-export default like;
+// export default like;

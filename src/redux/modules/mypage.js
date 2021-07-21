da9c7a9593a1d0e43d562fbc9bc237e8bca8e77a
sunm-in <sunm309@naver.com>
2021-07-22 04:13:25 +0900

    follower, morePost 중간작업

--- src/redux/modules/mypage.js
+++ src/redux/modules/mypage.js
@@ -18,7 +18,7 @@ const getMyPagePost = createAction(GET_MYPAGE_POST, (list, start) => ({
   list,
   start,
 }));
-const getMyPageMorePost = createAction(GET_MYPAGE_MORE_POST, (list, start) => ({
+const getMoreMyPost = createAction(GET_MYPAGE_MORE_POST, (list, start) => ({
   list,
   start,
 }));
@@ -26,7 +26,7 @@ const getMyPageLike = createAction(GET_MYPAGE_LIKE, (likeList, start) => ({
   likeList,
   start,
 }));
-const getMyPageMoreLike = createAction(
+const getMoreMyLike = createAction(
   GET_MYPAGE_MORE_LIKE,
   (likeList, start) => ({ likeList, start })
 );
@@ -43,6 +43,8 @@ const getMyPageFollowing = createAction(
 const initialState = {
   list: [],
   likeList: [],
+  followerList: [],
+  followingList: [],
   start: 0,
 };
 
@@ -66,12 +68,35 @@ const getMyPostDB = (limit = 5) => {
   };
 };
 
+const getMoreMyPostDB = (limit = 5) => {
+  return function (dispatch, getState) {
+    const start = getState().post.start;
+
+    if (start === null) return;
+
+    instance
+      .get(`/api/post/user?start=${start}&limit${limit + 1}`)
+      .then((res) => {
+        if (res.data.result.length < limit + 1) {
+          dispatch(getMoreMyPost(res.data.result, null));
+          return
+        }
+
+        res.data.result.pop();
+        dispatch(getMoreMyPost(res.data.result, start + limit));
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
+}
+
 const getMyLikeDB = (limit = 5) => {
   return function (dispatch, getState, { history }) {
     instance
       .get(`/api/post/like?start=0&limit=${limit + 1}`)
       .then((res) => {
-        console.log(res)
+        // console.log(res)
         if (res.data.result.length < limit + 1) {
           dispatch(getMyPageLike(res.data.result, null));
           return;
@@ -81,10 +106,29 @@ const getMyLikeDB = (limit = 5) => {
         dispatch(getMyPageLike(res.data.result, null));
       })
       .catch((error) => {
-        console.log(error);
+        console.error(error);
       });
   };
 };
+
+const getMyFollowerDB = (limit = 5) => {
+  return function (dispatch, getState, { history }) {
+    instance
+      .get(`/api/user/follower?start=0&limit=${limit + 1}`)
+      .then((res) => {
+        if (res.data.result.length < limit + 1) {
+          dispatch(getMyPageFollower(res.data.result, null));
+          return;
+        }
+
+        res.data.result.pop();
+        dispatch(getMyPageFollower(res.data.result, null));
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
+}
 
 // REDUCER
 export default handleActions(
@@ -95,18 +139,36 @@ export default handleActions(
         draft.start = action.payload.start;
       }),
 
+    [GET_MYPAGE_MORE_POST]: (state, action) =>
+    produce(state, (draft) => {
+      draft.list.push(...action.payload.list)
+      draft.start = action.payload.start;
+      // draft.list.push(...action.payload.post_list);
+      //   draft.paging = action.payload.paging;
+      //   draft.is_loading = false;
+      // return { ...state, list: [...state.list, ...action.postList], start: action.start };
+    }),
+
     [GET_MYPAGE_LIKE]: (state, action) =>
       produce(state, (draft) => {
         draft.likeList = action.payload.likeList;
         draft.start = action.payload.start;
       }),
+
+    [GET_MYPAGE_FOLLOWER]: (state, action) =>
+      produce(state, (draft) => {
+        draft.followerList = action.payload.followerList;
+        draft.start = action.payload.start;
+      })
   },
   initialState
 );
 
 const myPageActions = {
   getMyPostDB,
+  getMoreMyPost,
   getMyLikeDB,
+  getMyFollowerDB,
 };
 
 export { myPageActions };

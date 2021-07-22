cd332f68095fe08ba74f46a7d5064aee6859e506
sunm-in <sunm309@naver.com>
2021-07-22 16:00:21 +0900

    follower, following 위치 수정

--- src/pages/Following.js
+++ src/pages/Following.js
@@ -7,7 +7,7 @@ import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import { flexBox } from "../common/style";
 
 // ELEMENTS
-import { Grid, Text, Input, Button, Image } from "../elements";
+import { Grid, Text, Input, Button } from "../elements";
 
 // COMPONENTS
 import BlogUser from "../components/BlogUser";
@@ -15,7 +15,6 @@ import Permit from "../components/Permit";
 import MyFollowing from "../components/MyFollowing";
 
 // ICON
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 // REDUX
 import { myPageActions } from "../redux/modules/mypage";
@@ -95,159 +94,14 @@ const Following = (props) => {
 
           {/* 구분선 -------------------------------- */}
 
-              {followingList.map((following) => {
-                return <MyFollowing post={following} key={(Date.now() + Math.random()).toString(36)} />
-              })}
-          
-          {/* <Grid margin="2% 0 0 0">
-            <Grid
-              padding="12px"
-              bgColor="white"
-              addstyle={() => {
-                return css`
-                  display: flex;
-                  align-items: center;
-                `;
-              }}
-            >
-              <Image
-                addstyle={() => {
-                  return css`
-                    width: 36px;
-                  `;
-                }}
-                height="36px"
-                src="https://assets.tumblr.com/images/default_avatar/octahedron_open_128.png"
+          {followingList.map((following) => {
+            return (
+              <MyFollowing
+                post={following}
+                key={(Date.now() + Math.random()).toString(36)}
               />
-
-              <Grid
-                magin="0 0 3% 0"
-                addstyle={() => {
-                  return css`
-                    display: flex;
-                    flex-direction: column;
-                    padding: 4px;
-                  `;
-                }}
-              >
-                <Text
-                  fontSize="13px"
-                  fontWeight="bold"
-                  addstyle={() => {
-                    return css`
-                      padding: 2px;
-                    `;
-                  }}
-                >
-                  hangulteam
-                </Text>
-
-                <Text
-                  fontSize="14px"
-                  fontWeight="300"
-                  addstyle={() => {
-                    return css`
-                      padding: 2px;
-                    `;
-                  }}
-                >
-                  5일 전 업데이트됨
-                </Text>
-              </Grid>
-
-              <Text
-                margin="0 -30% 0 30%"
-                fontSize="14px"
-                color="blue"
-                addstyle={() => {
-                  return css`
-                    padding: 2px;
-                    width: 100%;
-                  `;
-                }}
-              >
-                언팔로우
-              </Text>
-
-              <MoreVertIcon />
-            </Grid>
-          </Grid> */}
-
-          {/* 구분선 -------------------------------- */}
-
-          {/* <Grid margin="0 0 0 0">
-            <Grid
-              padding="12px"
-              bgColor="white"
-              addstyle={() => {
-                return css`
-                  display: flex;
-                  align-items: center;
-                `;
-              }}
-            >
-              <Image
-                addstyle={() => {
-                  return css`
-                    width: 36px;
-                  `;
-                }}
-                height="36px"
-                src="https://assets.tumblr.com/images/default_avatar/octahedron_open_128.png"
-              />
-
-              <Grid
-                magin="0 0 3% 0"
-                addstyle={() => {
-                  return css`
-                    display: flex;
-                    flex-direction: column;
-                    padding: 4px;
-                  `;
-                }}
-              >
-                <Text
-                  fontSize="13px"
-                  fontWeight="bold"
-                  addstyle={() => {
-                    return css`
-                      padding: 2px;
-                    `;
-                  }}
-                >
-                  staff
-                </Text>
-
-                <Text
-                  fontSize="14px"
-                  fontWeight="300"
-                  addstyle={() => {
-                    return css`
-                      padding: 2px;
-                    `;
-                  }}
-                >
-                  5일 전 업데이트됨
-                </Text>
-              </Grid>
-
-              <Text
-                margin="0 -30% 0 30%"
-                fontSize="14px"
-                color="blue"
-                addstyle={() => {
-                  return css`
-                    padding: 2px;
-                    width: 100%;
-                  `;
-                }}
-              >
-                언팔로우
-              </Text>
-
-              <MoreVertIcon />
-            </Grid>
-          </Grid> */}
+            );
+          })}
         </Grid>
 
         {/* 구분선 -------------------------------- */}
